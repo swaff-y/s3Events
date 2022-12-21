3de61c2d3e30dc78e365cd77e0c08c566aea9f8e
swaff-y <kyle@swaff.id.au>
2022-12-21 23:02:17 +1100

    Added an event Processing class

---
+++ helpers/eventProcessor.js
@@ -0,0 +1,44 @@
+const Logger = require('../helpers/logger');
+
+module.exports = class EventProcessor{
+  constructor(event){
+    this.event = event;
+    try{
+      this.processEvent();
+    } catch(error) {
+      new Logger('Error', error);
+    }
+  }
+
+  processEvent(){
+    const event = this.event;
+    const records = event.Records;
+    const regexp = /ObjectCreated/gi;
+    this.data = {};
+
+    if(records && Array.isArray(records)) {
+      records.forEach((record) => {
+        const body = record.body
+        const recs = JSON.parse(body).Records
+        if(recs && Array.isArray(recs)) {
+          recs.forEach((rec) => {
+            const eventName = rec.eventName;
+            const s3 = rec.s3;
+            const s3Object = s3.object
+            const eventKey = s3Object.key
+            const data = { 
+              type: eventName,
+              s3: s3Object,
+              key: eventKey
+            }
+            if(eventName.match(regexp)){
+              data.size = s3Object.size;
+              data.tag = s3Object.eTag;
+            }
+            return this.data = data;
+          })
+        }
+      })
+    }
+  }
+}
--- lambdas/s3EventHandler.js
+++ lambdas/s3EventHandler.js
@@ -1,29 +1,7 @@
-exports.handler = event => {
-  console.log('event => ', event)
-  const records = event.Records;
+const Logger = require('../helpers/logger');
+const EventProcessor = require('../helpers/eventProcessor');
 
-  if(records && Array.isArray(records)) {
-    records.forEach((record) => {
-      const body = record.body
-      const recs = JSON.parse(body).Records
-      if(recs && Array.isArray(recs)) {
-        recs.forEach((rec) => {
-          const eventName = rec.eventName;
-          const s3 = rec.s3;
-          const s3Object = s3.object
-          const eventKey = s3Object.key
-          const data = { 
-            type: eventName,
-            s3: s3Object,
-            key: eventKey
-          }
-          if(eventName == 'ObjectCreated:Put'){
-            data.size = s3Object.size;
-            data.tag = s3Object.eTag;
-          }
-          console.log('Data: ', data);
-        })
-      }
-    })
-  }
+exports.handler = event => {
+  const eventProcessor = new EventProcessor(event);
+  new Logger('Info', eventProcessor.data);
 }
